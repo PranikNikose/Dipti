3692f31356176d0c33f996c5e81a4159db26ee42
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2022-10-24 19:13:14 +0530

    Delete file-upload-to-atlas directory

--- file-upload-to-atlas/src/main/java/net/pranik/spring/mongodb/repositoires/PhotoRepositirys.java
+++
@@ -1,9 +0,0 @@
-package net.pranik.spring.mongodb.repositoires;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import net.pranik.spring.mongodb.modelss.PhotoModel;
-
-public interface PhotoRepositirys extends MongoRepository<PhotoModel, String>
-{
-
-}
